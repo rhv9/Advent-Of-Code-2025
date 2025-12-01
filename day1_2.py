f0a767e3102d3a87a0848fb73bece6753fe76e33
rhv9 <rahulhasmukv@gmail.com>
2025-12-01 23:35:53 +0000

    Unga bunga way.

--- day1_2.py
+++ day1_2.py
@@ -1,4 +1,5 @@
-print("Hello WOrld!")
+import time
+
 
 f = open("input/1.txt")
 inputText = f.read()
@@ -7,26 +8,27 @@ inputList = inputText.splitlines()
 dial:int = 50
 result:int = 0
 
+start = time.time()
 for line in inputList:
     count = 0
-    if line[0] == 'R':
-        num = int(line[1:])
-        dial += num
-        count = int(dial / 100)
-        dial = dial % 100
-    else :
-        num = int(line[1:])
-        prevDial = dial
-        dial -= num
-        if dial < 0 and prevDial != 0:
-            count += 1
-        count += abs(int(dial / 100))
-        dial = dial % 100
+    sign = 1 if line[0] == 'R' else -1
+    num = int(line[1:])
+    prevDial = dial
+    dial += num * sign
+    if sign == -1 and dial < 0 and prevDial != 0:
+        count += 1
 
-    if dial == 0 and count > 0:
-        count -= 1 
+    count += abs(int(dial / 100))
+    dial = dial % 100
+
     if dial == 0:
         result += 1
+        # Remove extra count
+        if (count > 0):
+            count -= 1  
+
     result += count
+end = time.time()
 
 print("Result: ", result)
+print((end - start) * 1000, "ms")
--- day1_ungabunga.py
+++ day1_ungabunga.py
@@ -9,14 +9,24 @@ result:int = 0
 start = time.time()
 for line in inputList:
     sign = 1 if line[0] == 'R' else -1
-    num = int(line[1:])
 
-    dial += num * sign
-    dial = dial % 100
+    if line[0] == 'R':
+        num = int(line[1:])
+        for _ in range(0, num):
+            dial += 1
+            if dial > 99:
+                dial = 0
+            
+    else:
+        num = int(line[1:])
+        for _ in range(0, num):
+            dial -= 1
+            if dial < 0:
+                dial = 99
 
     if dial == 0:
         result += 1
-        
 end = time.time()
+
 print("Result: ", result)
 print((end - start) * 1000, "ms")
